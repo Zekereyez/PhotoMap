18dcd2cb3dd9dcabee3abd943d4697ea1cf5a944
Ezequiel Reyes <reyeseze@usc.edu>
2022-06-29 13:51:00 -0700

    Add files via upload
    Completed Photomap Code

---
+++ fbu_ios_photomap_objc-starter-project/PhotoMap/View Controllers/PhotoMapViewController.h
@@ -0,0 +1,20 @@
+//
+//  PhotoMapViewController.h
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import "LocationsViewController.h"
+//#import "UIKit"
+//#import "MapKit"
+//#import "CoreLocation"
+//#import "AddressBookUI"
+//#import "MessageUI"
+
+@interface PhotoMapViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MKMapViewDelegate>
+
+@end
